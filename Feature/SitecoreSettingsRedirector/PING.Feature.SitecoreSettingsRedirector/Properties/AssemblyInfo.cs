dd9ebeccba1b59b68aea980a6397dc87b3920f72
Richard Hauer <richard.hauer@ping-works.com.au>
2018-08-13 12:24:28 +1000

    Update assembly versions

--- Feature/SitecoreSettingsRedirector/PING.Feature.SitecoreSettingsRedirector/Properties/AssemblyInfo.cs
+++ Feature/SitecoreSettingsRedirector/PING.Feature.SitecoreSettingsRedirector/Properties/AssemblyInfo.cs
@@ -2,37 +2,20 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("PING.Feature.SitecoreSettingsRedirector")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PING.Feature.SitecoreSettingsRedirector")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle( "PING Works Sitecore Settings Redirector" )]
+[assembly: AssemblyDescription( "Feature to allow override of Sitecore Settings to AppSettings - particularly useful for environment-related settings in Azure like Solr connection strings" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "PING Works" )]
+[assembly: AssemblyProduct( "PING Works Sitecore Settings Redirector" )]
+[assembly: AssemblyCopyright( "Copyright ©  2018" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible( false )]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2c50a5ca-81fc-417c-aee0-692c8ad97d19")]
+[assembly: Guid( "2c50a5ca-81fc-417c-aee0-692c8ad97d19" )]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "1.1.*" )]
+[assembly: AssemblyFileVersion( "1.1.0.0" )]
 
-[assembly:InternalsVisibleTo("PING.Feature.SitecoreSettingsRedirector.Test")]
+[assembly: InternalsVisibleTo( "PING.Feature.SitecoreSettingsRedirector.Test" )]
